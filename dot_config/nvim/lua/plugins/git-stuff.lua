3debf5a742eb8b7a052bd9cd7c116780087363e0
Josh DeWitt <josh.dewitt@skydio.com>
2026-09-05 15:51:15 -0700

    chore: [nvim] git stuff update, more consistent keymaps
    let [h and ]h be what they should be, move Gitsigns v Git cmds where they should be

--- dot_config/nvim/lua/plugins/git-stuff.lua
+++ dot_config/nvim/lua/plugins/git-stuff.lua
@@ -1,6 +1,24 @@
 return {
   {
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    cmd = { "Git", "Gdiffsplit", "Gvdiffsplit", "Gread", "Gwrite" },
+    keys = {
+      { "<leader>gd", "<cmd>Gvdiffsplit<cr>" },
+      { "<leader>gs", "<cmd>vertical topleft Git<cr>" },
+      { "<leader>gl", "<cmd>vertical topleft Git log --oneline -n 40<cr>" },
+      { "gdh",        "<cmd>diffget //2<cr>" }, -- from 2, rhs, "head"
+      { "gdl",        "<cmd>diffget //3<cr>" }, -- from 3, lhs
+    },
+    config = function()
+      -- fugitive always opens its editor (commit msg, rebase todo) in a plain
+      -- split; move it to a full-height column on the right instead
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "FugitiveEditor",
+        callback = function()
+          vim.cmd("wincmd L")
+        end,
+      })
+    end,
   },
   {
     "lewis6991/gitsigns.nvim",
@@ -10,9 +28,13 @@ return {
       vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
       vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>", {})
-      vim.keymap.set("n", "<leader>gs", ":vertical topleft Git<CR>", {})
-      vim.keymap.set('n', "<leader>gn", ':Gitsigns next_hunk<CR>')
-      vim.keymap.set('n', "<leader>gp", ':Gitsigns prev_hunk<CR>')
+      vim.keymap.set("n", "<leader>hh", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set('n', "]h", ':Gitsigns nav_hunk next --target=all<CR>')
+      vim.keymap.set('n', "[h", ':Gitsigns nav_hunk prev --target=all<CR>')
+      vim.keymap.set('n', "]H", ':Gitsigns nav_hunk next --target=unstaged<CR>')
+      vim.keymap.set('n', "[H", ':Gitsigns nav_hunk prev --target=unstaged<CR>')
+      vim.keymap.set('n', "<leader>hs", ':Gitsigns stage_hunk<CR>')
+      vim.keymap.set('n', "<leader>hr", ':Gitsigns reset_hunk<CR>')
     end
   },
   {
@@ -31,7 +53,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        { "<leader>gw", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
     }
 }
