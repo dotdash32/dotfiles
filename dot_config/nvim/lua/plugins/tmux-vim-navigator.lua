478954e9ca197f776ec96ae0c0478cdf4183bc07
Josh DeWitt <josh.dewitt@skydio.com>
2026-09-05 16:07:02 -0700

    feat: [nvim/tmux] switch to smart-splits
    nvim-tmux-navigation wasn't handling my resizing, so switch to a 'newer' (?) plugin

--- dot_config/nvim/lua/plugins/tmux-vim-navigator.lua
+++ dot_config/nvim/lua/plugins/tmux-vim-navigator.lua
@@ -1,10 +1,31 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    require('nvim-tmux-navigation').setup({})
-    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
-    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
-    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
-    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
-  end,
+  -- "alexghergh/nvim-tmux-navigation", doesn't do splits nicelw, so it's a misnamed file.
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    config = function()
+      local ss = require("smart-splits")
+      ss.setup({
+        ignored_filetypes = { "neo-tree" }, -- edge detection for resize only
+      })
+      
+      -- move around panes
+      vim.keymap.set("n", "<C-h>", ss.move_cursor_left)
+      vim.keymap.set("n", "<C-j>", ss.move_cursor_down)
+      vim.keymap.set("n", "<C-k>", ss.move_cursor_up)
+      vim.keymap.set("n", "<C-l>", ss.move_cursor_right)
+
+      -- resizing
+      vim.keymap.set('n', '<A-C-h>', ss.resize_left)
+      vim.keymap.set('n', '<A-C-j>', ss.resize_down)
+      vim.keymap.set('n', '<A-C-k>', ss.resize_up)
+      vim.keymap.set('n', '<A-C-l>', ss.resize_right)
+
+      -- move windows
+      vim.keymap.set('n', '<leader>H', ss.swap_buf_left)
+      vim.keymap.set('n', '<leader>J', ss.swap_buf_down)
+      vim.keymap.set('n', '<leader>K', ss.swap_buf_up)
+      vim.keymap.set('n', '<leader>L', ss.swap_buf_right)
+    end,
+  },
 }
